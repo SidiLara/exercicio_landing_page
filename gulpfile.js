33976786c0a0cfbe229badd1b9bce8a0f47117e4
Sidinei Lara <sidineilara@gmail.com>
2024-06-04 22:21:48 +0100

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles() {
+    return gulp
+        .src('src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function watch() {
+    gulp.watch('src/styles/*.scss', styles);
+}
+
+exports.default = gulp.series(styles, watch);
+exports.watch = watch;
